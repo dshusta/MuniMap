2e400e308eb71a69ea98d004ed108ea940db389e
Daniel Shusta <dshusta@pivotal.io>
2014-11-18 19:23:28 -0800

    Test drive map starting position.

--- MuniMap Tests/RootViewControllerSpec.swift
+++ MuniMap Tests/RootViewControllerSpec.swift
@@ -1,10 +1,27 @@
-import UIKit
 import Quick
 import Nimble
+import UIKit
+import MapKit
 
 class RootViewControllerSpec: QuickSpec {
     override func spec() {
+        var subject : RootViewController!
+        beforeEach {
+            subject = RootViewController()
+            expect(subject.view).toNot(beNil())
+        }
+
+        it("have a mapview that starts in San Francisco") {
+            expect(subject.mapview).toNot(beNil())
+
+            let buenaVistaPark = CLLocationCoordinate2DMake(37.768783, -122.442113);
+            let buenaVistaPoint = MKMapPointForCoordinate(buenaVistaPark)
+            let visibleMapRect = subject.mapview.visibleMapRect
+            expect(MKMapRectContainsPoint(visibleMapRect, buenaVistaPoint)).to(beTruthy())
+
+            let vegas = CLLocationCoordinate2DMake(36.168773, -115.146505)
+            let vegasPoint = MKMapPointForCoordinate(vegas)
+            expect(MKMapRectContainsPoint(visibleMapRect, vegasPoint)).to(beFalsy())
+        }
     }
-
-
 }
--- MuniMap/RootViewController.swift
+++ MuniMap/RootViewController.swift
@@ -2,10 +2,15 @@ import UIKit
 import MapKit
 
 class RootViewController: UIViewController {
-    @IBOutlet weak var mapview: MKMapView!
+    var mapview : MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        mapview = MKMapView(frame: view.bounds)
+        mapview.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        view.addSubview(mapview)
+
         let span = MKCoordinateSpanMake(0.25, 0.25);
         let buenaVistaPark = CLLocationCoordinate2DMake(37.768783, -122.442113);
         let region = MKCoordinateRegionMake(buenaVistaPark, span);
