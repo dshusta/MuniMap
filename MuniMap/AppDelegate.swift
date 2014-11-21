ec7aef37a1c538994fd4685a9652fbaafc283ae9
Daniel Shusta <dshusta@pivotal.io>
2014-11-20 20:50:11 -0800

    Initialize window in AppDelegate, remove reference to storyboard.

--- MuniMap/AppDelegate.swift
+++ MuniMap/AppDelegate.swift
@@ -7,7 +7,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        let rootViewController = RootViewController(nibName: nil, bundle: nil)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+
         return true
     }
 
